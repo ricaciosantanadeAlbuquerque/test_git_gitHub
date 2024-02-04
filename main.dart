2695abc9594530f3f6f96f089a86f84d7caa7267
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-02-03 23:05:34 -0300

    Função Soma criada com Sucesso

--- main.dart
+++ main.dart
@@ -40,7 +40,9 @@ void opcao({required int op}) {
           try {
             num num1 = num.tryParse(entradaValor1) ?? 0;
             num num2 = num.tryParse(entradaValor2) ?? 0;
-            print('soma ${num1 + num2}');
+
+            soma(valor1: num1, valor2: num2);
+            
           } catch (e) {
             throw Exception('Erro ! não é possível Fazer a converção já que esteve valor não é um número');
           }
@@ -118,8 +120,6 @@ void opcao({required int op}) {
 
 // =========================================================================================================
 
-void soma({required num valor1, required num valor2}){
-   
-   print('O resultado da Soma é ${valor1 + valor2}');
-
+void soma({required num valor1, required num valor2}) {
+  print('O resultado da Soma é ${valor1 + valor2}');
 }
